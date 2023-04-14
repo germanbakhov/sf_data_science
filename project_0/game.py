a0b99842539954a046831687a264dc5bdf62aca8
German Bakhov <germanbakhov@gmail.com>
2023-04-14 05:03:46 +0400

    edit game_v2.py

--- project_0/game.py
+++ project_0/game.py
@@ -1,19 +1,25 @@
+"""Игра угадай число"""
+
 import numpy as np
 
 number = np.random.randint(1, 101) # загадываем число
 
+# количество попыток
 count = 0
 
 while True:
-    count += 1
-    predict_number = int(input("Угадай число от 1 до 100"))
-
+    count+=1
+    predict_number = int(input("Угадай число от 1 до 100: "))
+    
     if predict_number > number:
         print("Число должно быть меньше!")
 
     elif predict_number < number:
         print("Число должно быть больше!")
-
+        
+    elif count == 20:
+        break  # выход из цикла если угадали    
+    
     else:
         print(f"Вы угадали число! Это число = {number}, за {count} попыток")
-        break # конец игры, выход из цикла
+        break #конец игры выход из цикла
